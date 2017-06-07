7be457ac1324651e271ae32203568f05101a08a4
shinmike <shinmike90@gmail.com>
2017-06-07 19:26:55 +0000

    Task 2: Install the Module - cookie-parser

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+app.use(cookieParser());
 
 app.use("/assets", express.static("assets")); // to apply CSS
 
